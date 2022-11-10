90b82b05408c44a861fd8cfc58411752eb7aedab
Pavel <pavel07007@gmail.com>
2022-11-10 14:10:38 +0300

    try scc module on social

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from "./Header.module.css";
 
 const Header = () => {
     return (
-            <header className='header'>
+            <header className={classes.header}>
                 <img src="https://point.bangbangeducation.ru/img/sbMRG5DK7O-2MNbywcvgS1IPqbmj_h1atA9UMOLTW6M/fill/746/560/ce/1/bG9jYWw6Ly9zdG9y/ZS81YWE4M2U0OWJh/Mjk2MTVmYTQ0MDFk/ODVjODM1ZDBkMzk2/MDQ3ZTE3MGM1OTRl/NmJlNzNjOGYxNDQ0/ZDY.png" alt=""/>
             </header>
     );

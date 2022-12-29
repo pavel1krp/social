b8eb58fe3f5adae36202b727eb48aff3c7f6834f
Pavel <pavel07007@gmail.com>
2022-12-29 15:49:46 +0300

    change

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,77 +1,15 @@
 import {v1} from "uuid";
-import {MessagesDataType} from "../App";
-import {profileReducer} from "./profile-reducer.js";
-import {dialogReducer} from "./dialogs-reducer.js";
-
-export type postDatapropsType = {
-    id: string
-    message: string
-    name: string
-    likesCount: number
-    src: string
-}
-export type DialogPageType = {
-    dialogsData: DialogDataType[]
-    messagesData: MessagesDataType[]
-    newMessageBody:string
-}
-export type DialogDataType = {
-    id: string
-    name: string
-}
-
-export type ProfilePageType = {
-    postData: postDatapropsType[]
-    newPostText: string
-
-}
-export type StatePropsType = {
-    ProfilePage: ProfilePageType
-    DialogPage: DialogPageType
-}
-
-export type AddPOstActionType = {
-    type: "ADD-POST"
-}
-
-export type ChangeNewTextActionType = {
-    type: 'UPDATE-NEW-POST-TEXT'
-    newText: string
-}
-
-export type AddMessageActionType = {
-    type: "ADD-MESSAGE"
-}
-
-export type ChangeNewMessageActionType = {
-    type: 'UPDATE-NEW-MESSAGE-TEXT'
-    body: string
-}
-
-
-
-export type StoreType = {
-    _state: StatePropsType
-    _rerenderTree: () => void
-    subscribe: (observer: () => void) => void
-    getState: () => StatePropsType
-    dispatch: (action: ActionsType) => void
-}
-
-export type ActionsType = AddPOstActionType | ChangeNewTextActionType|AddMessageActionType| ChangeNewMessageActionType
-
-// const ADD_POSt = "ADD-POST";
-// const UPDATE_NEW_POST_TEXT ="UPDATE-NEW-POST-TEXT";
-// const ADD_MESSAGE = 'ADD-MESSAGE';
-// const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
-
-const store: StoreType = {
-    _state: {
-        ProfilePage: {
+import { MessagesDataType} from "../App";
+import {profileReducer} from "./profileReducer";
+import {dialogReducer} from "./dialogReducer";
+import {ActionType, StatePropsType, StoreType} from "../Types/types";
+export const store:StoreType = {
+    _state:{
+        profilePage: {
             postData: [
                 {
                     id: v1(),
-                    message: 'VKus',
+                    message: 'Вкусно',
                     name: 'Dasha',
                     likesCount: 11111,
                     src: 'https://klike.net/uploads/posts/2019-03/1551511801_1.jpg'
@@ -97,12 +35,10 @@ const store: StoreType = {
                     likesCount: 15,
                     src: 'https://klike.net/uploads/posts/2019-03/1551511808_5.jpg'
                 },
-
             ],
-            newPostText: 'Hello It',
-
+            newPostText:'',
         },
-        DialogPage: {
+        dialogPage: {
             dialogsData: [
                 {id: v1(), name: 'Pasha'},
                 {id: v1(), name: 'Dasha'},
@@ -120,25 +56,22 @@ const store: StoreType = {
                 {id: v1(), message: 'Hiushki'},
                 {id: v1(), message: 'Bye'},
             ],
-            newMessageBody: '',
-        }
+            newMessageText: '',
+        },
+        sideBar:{}
     },
-    _rerenderTree() {
+    _callSubscriber (state: StatePropsType){
+        console.dir('state')
     },
-    subscribe(observer: () => void) {
-        this._rerenderTree = observer
-    },
-    getState() {
+    getState(){
         return this._state
     },
-    dispatch(action) {
-        console.log(action.type)
-        this._state.ProfilePage = profileReducer(this._state.ProfilePage,action);
-        this._rerenderTree();
-        this._state.DialogPage= dialogReducer(this._state.DialogPage ,action);
-        this._rerenderTree();
+    subscribe(observer:(state: StatePropsType)=>void){
+         this._callSubscriber= observer
+    },
+    dispatch(action:ActionType){
+        this._state.profilePage = profileReducer(this._state.profilePage, action)
+        this._callSubscriber(this._state)
+        this._state.dialogPage = dialogReducer(this._state.dialogPage, action)
     }
 }
-
-export default store
-
